14c69485875bd014b2f80fbd7a7bf840bebda2ec
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-02-26 16:08:35 +0200

    change background,create course function added

--- js.js
+++ js.js
@@ -89,6 +89,32 @@ function mergeSort(array) {
       return true;
     }
   }
-  
 
-  
+
+  //create course thing
+
+  class Course {
+    constructor(title, instructor, duration) {
+      this.title = title;
+      this.instructor = instructor;
+      this.duration = duration;
+    }
+  }
+    const titleInput = document.getElementById('title');
+    const instructorInput = document.getElementById('instructor');
+    const durationInput = document.getElementById('duration');
+    const submitButton = document.getElementById('submit-button');
+
+    submitButton.addEventListener('click', () => {
+        
+        const title = titleInput.value;
+        const instructor = instructorInput.value;
+        const duration = parseInt(durationInput.value);
+
+        const course = new Course(title, instructor, duration);
+        console.log(course);
+
+        titleInput.value = '';
+        instructorInput.value = '';
+         durationInput.value = '';
+        });

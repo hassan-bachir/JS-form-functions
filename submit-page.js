1d4906c6b79b4e7b162512d3ec6d21abb46ab3fc
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-02-26 23:48:03 +0200

    added resposive js to other page

--- submit-page.js
+++ submit-page.js
@@ -1,5 +1,12 @@
 
 
+//resposive navigation toggle button
+const toggle =document.querySelector('.toggle')
+const toggleic =document.querySelector('.toggle i')
+const dropdown =document.querySelector('.dropdown')
+toggle.onclick = function(){
+    dropdown.classList.toggle('open');
+    const isopen =dropdown.classList.contains('open');}
 
  //create course thing
 

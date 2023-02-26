27d5c1ed5c98c01ff561f4af706413af5545a598
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-02-26 13:59:01 +0200

    implemented the merge sort js

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 
-// navigation toggle button
+//resposive navigation toggle button
 const toggle =document.querySelector('.toggle')
 const toggleic =document.querySelector('.toggle i')
 const dropdown =document.querySelector('.dropdown')
@@ -8,5 +8,53 @@ toggle.onclick = function(){
     const isopen =dropdown.classList.contains('open');}
     
 //json file things
-const fs = require('fs');
-const data = fs.readFileSync('data.json');
+// const fs = require('fs');
+// const data = fs.readFileSync('data.json');
+
+
+//mergesort()
+
+let a = [5,3,8,2,4,3,6,9]
+
+function mergeSort(array) {
+    if (array.length <= 1) {
+      return array;
+    }
+    
+    const middleIndex = Math.floor(array.length / 2);
+    const leftArray = array.slice(0, middleIndex);
+    const rightArray = array.slice(middleIndex);
+  
+    const sortedLeft = mergeSort(leftArray);
+    const sortedRight = mergeSort(rightArray);
+    
+    return merge(sortedLeft, sortedRight);
+  }
+  
+  function merge(leftArray, rightArray) {
+    const resultArray = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+  
+    while (leftIndex < leftArray.length && rightIndex < rightArray.length) {
+      if (leftArray[leftIndex] < rightArray[rightIndex]) {
+        resultArray.push(leftArray[leftIndex]);
+        leftIndex++;
+      } else {
+        resultArray.push(rightArray[rightIndex]);
+        rightIndex++;
+      }
+    }
+    
+    return resultArray
+      .concat(leftArray.slice(leftIndex))
+      .concat(rightArray.slice(rightIndex));
+  }
+    
+    
+    console.log(mergeSort(a));
+    
+
+
+
+  

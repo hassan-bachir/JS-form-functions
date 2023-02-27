1a47c0e079363e3e72bc9999a431e14c9bcc3aac
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-02-27 03:19:07 +0200

    finished probably

--- sign-up.js
+++ sign-up.js
@@ -1,3 +1,36 @@
+//SUBMIT RULES
+
+const firstNameInput = document.querySelector('#first-name');
+const lastNameInput = document.querySelector('#last-name');
+const emailInput = document.querySelector('#email');
+const bDateInput = document.querySelector('#b-date');
+const passwordInput = document.querySelector('#password');
+
+const submitButton = document.querySelector('#submit-button');
+
+submitButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    if (firstNameInput.value.trim() === '') {
+        alert('Please enter your first name');
+        return;
+      }
+      if (lastNameInput.value.trim() === '') {
+        alert('Please enter your last name');
+        return;
+      }
+      const emailRegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegExp.test(emailInput.value)) {
+        alert('Please enter a valid email address');
+        return;
+      }
+     const arr =passwordInput.value.split('')
+      if (arr.length <8 ) {
+        alert('Please enter a valid password');
+        return;
+      }
+      alert('Form submitted successfully');
+    });        
+
 //json file things
 const submitBtn = document.getElementById("submit-button");
 
